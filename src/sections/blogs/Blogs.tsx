d79a472c3a9d5fdc5553a71d97125c93ae9f18e5
RRCris <curiosity.cdrr@gmail.com>
2025-02-14 16:05:42 -0500

    add blogs section

--- src/sections/blogs/Blogs.tsx
+++ src/sections/blogs/Blogs.tsx
@@ -1,4 +1,5 @@
-import response_API from "../../@Types/API";
+import response_API, { Blog } from "../../@Types/API";
+import Image from "../../components/image/Image";
 import Spinner from "../../components/spinner/Spinner";
 import useGetAPI from "../../hooks/useGetAPI";
 import css from "./Blogs.module.css";
@@ -8,7 +9,34 @@ export default function Blogs() {
   return (
     <section className={css.container} id="blogs">
       <h2>BLOGS</h2>
-      {data ? <Spinner className={css.spinner} /> : <h2>ha</h2>}
+      {data ? (
+        <div className={css.containerCards}>
+          {data.map((blog, index) => (
+            <CardBlog data={blog} main={index === 0} />
+          ))}
+        </div>
+      ) : (
+        <Spinner className={css.spinner} />
+      )}
     </section>
+  );
+}
+
+function CardBlog({ data, main }: { data: Blog; main?: boolean }) {
+  if (main)
+    return (
+      <div className={css.containerMainCard}>
+        <Image width={350} height={350} src={data.image_cover} />
+        <h3>{data.title}</h3>
+        <p>{data.description}</p>
+        <a href={data.url_front}>LEER MAS</a>
+      </div>
+    );
+  return (
+    <div className={css.containerCard}>
+      <Image src={data.image_cover} />
+      <h3>{data.title}</h3>
+      <a href={data.url_front}>LEER MAS</a>
+    </div>
   );
 }
